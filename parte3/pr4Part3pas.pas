94660274f787f39da8a3f56fd46d717a3b287a8c
leandro <leandrodavid13@hotmail.com>
2023-05-31 17:47:36 -0300

    pr6

---
+++ parte3/pr4Part3pas.pas
@@ -0,0 +1,12 @@
+(*
+4. Una compañía de telefonía celular debe realizar la facturación mensual de sus 9300 clientes con planes de
+consumo ilimitados (clientes que pagan por lo que consumen). Para cada cliente se conoce su código de cliente y
+cantidad de líneas a su nombre. De cada línea se tiene el número de teléfono, la cantidad de minutos consumidos
+y la cantidad de MB consumidos en el mes. Se pide implementar un programa que lea los datos de los clientes de
+la compañía e informe el monto total a facturar para cada uno. Para ello, se requiere:
+a. Realizar un módulo que lea la información de una línea de teléfono.
+b. Realizar un módulo que reciba los datos de un cliente, lea la información de todas sus líneas (utilizando el
+módulo desarrollado en el inciso a. ) y retorne la cantidad total de minutos y la cantidad total de MB a facturar
+del cliente.
+Nota: para realizar los cálculos tener en cuenta que cada minuto cuesta $3,40 y cada MB consumido cuesta $1,35.
+*)
